45dda7186800ff70d3608fbe402ee39787669904
adito <adrien.bourget@edu.ece.fr>
2018-04-07 10:18:34 +0200

    suppok

---
+++ suppokadri/ps2/affichage.h
@@ -0,0 +1,6 @@
+#ifndef AFFICHAGE_H_INCLUDED
+#define AFFICHAGE_H_INCLUDED
+
+void menu(struct BITMAP* fond_menu, bool* menu_state , bool* chaine1_ok, bool* chaine2_ok, bool* chaine3_ok, bool* maj, Graph& g1, Graph& g2, Graph& g3);
+
+#endif // AFFICHAGE_H_INCLUDED
